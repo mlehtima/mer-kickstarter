f0db61a9fe6ea7c4c442941da9d1efd669a21124
Marko Saukko <marko.saukko@jollamobile.com>
2012-08-01 10:08:42 +0000

    Fix version and point to Mer with URL.
    Signed-off-by: Marko Saukko <marko.saukko@jollamobile.com>

--- setup.py
+++ setup.py
@@ -8,12 +8,28 @@ try:
 except ImportError:
     pass
 
+version_file = 'VERSION'
+if not os.path.isfile(version_file):
+    print 'No %s file in topdir, abort' % (version_file)
+    sys.exit(1)
+
+try:
+    # first line should be the version number
+    version = open(version_file).readline().strip()
+    if not version:
+        print '%s file is invalid, abort' % (version_file)
+        sys.exit(1)
+except IOError:
+    print 'WARNING: Cannot write version number file'
+
+
+
 setup(name='kickstarter',
-      version = "0.1",
+      version = version,
       description='Kickstarter',
-      author='Anas Nashif',
-      author_email='anas.nashif@intel.com',
-      url='http://meego.com/',
+      author='Marko Saukko',
+      author_email='sage@merproject.org',
+      url='http://www.merproject.org/',
       scripts=['tools/kickstarter'],
       packages=['kickstart']
      )
